50fd785da922346896a4c5f76dd3b9b3c334c6ae
Jonathan <jonathan.ribeiro28121997@gmail.com>
2021-04-30 11:00:31 -0300

    Inicializacao do projeto integrador, model

---
+++ Rango/Rango/src/main/java/com/projetorango/Rango/RangoApplication.java
@@ -0,0 +1,13 @@
+package com.projetorango.Rango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RangoApplication.class, args);
+	}
+
+}
---
+++ Rango/Rango/src/test/java/com/projetorango/Rango/RangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetorango.Rango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
