4dde14e8a42c6b1beb7b45cd273f65df9fd94d10
Jonathan <jonathan.ribeiro28121997@gmail.com>
2021-05-12 16:16:13 -0300

    Implementação do security

---
+++ Rango/Rango/src/main/java/com/projetorango/Rango/controller/UsuarioController.java
@@ -0,0 +1,41 @@
+package com.projetorango.Rango.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetorango.Rango.model.Usuario;
+import com.projetorango.Rango.model.UsuarioLogin;
+import com.projetorango.Rango.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin("*")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@PostMapping("/logar")
+	public ResponseEntity<UsuarioLogin> Autentication(@RequestBody Optional<UsuarioLogin> user) {
+		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> post(@RequestBody Usuario usuario) {
+		Optional<Usuario> user = usuarioService.CadastrarUsuario(usuario);
+		try {
+			return ResponseEntity.ok(user.get());
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+}
