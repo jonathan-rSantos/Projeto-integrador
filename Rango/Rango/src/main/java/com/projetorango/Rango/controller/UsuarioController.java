5474f78e0b793804778f725107552535b663b766
Jonathan <jonathan.ribeiro28121997@gmail.com>
2021-06-09 09:52:34 -0300

    Atualizando controller usuario

--- Rango/Rango/src/main/java/com/projetorango/Rango/controller/UsuarioController.java
+++ Rango/Rango/src/main/java/com/projetorango/Rango/controller/UsuarioController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.projetorango.Rango.model.Usuario;
 import com.projetorango.Rango.model.UsuarioLogin;
+import com.projetorango.Rango.repository.UsuarioRepository;
 import com.projetorango.Rango.service.UsuarioService;
 
 @RestController
@@ -23,12 +26,22 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioService usuarioService;
 	
+	@Autowired
+    private UsuarioRepository repository;
+	
 	@PostMapping("/logar")
 	public ResponseEntity<UsuarioLogin> Autentication(@RequestBody Optional<UsuarioLogin> user) {
 		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
 
+	
+	@GetMapping("/{id}")
+    public ResponseEntity<Usuario> GetById(@PathVariable long id) {
+        return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+    }
+	
+	
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> post(@RequestBody Usuario usuario) {
 		Optional<Usuario> user = usuarioService.CadastrarUsuario(usuario);
